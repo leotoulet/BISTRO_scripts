7420ede09c82542afa7331532cdc145e76bd1822
leotoulet <leo_toulet@berkeley.edu>
2019-11-21 12:38:23 -0800

    Added price_area_KPI

---
+++ output_analysis/price_area_functions.py
@@ -0,0 +1,41 @@
+import matplotlib.pyplot as plt
+import numpy as np
+import os
+from math import *
+import sys 
+
+sys.path.append(os.path.abspath("../fixed_data/"))
+from KPIS import *
+
+sys.path.append(os.path.abspath("../utilities/"))
+from Sample import *
+from collect_inputs import *
+
+MIN_X = 676949
+MAX_X = 689624
+
+MIN_Y = 4818750
+MAX_Y = 4832294
+
+def KPI_wrt_price_area(samples, standards, KPI, name, folder):
+
+	plt.clf()
+
+	print("    Generating "+name+" wrt to price area")
+
+	price_area = [np.pi*s.road_pricing["r"]**2*s.road_pricing["p"] for s in samples]
+	scores = [computeWeightedScores(s, standards, KPI)[-1] for s in samples]
+
+	plt.title(name + "w.r.t price * area")
+	plt.x_label("Price * Area ($ * m^2)")
+	plt.y_label("KPI score")
+
+	plt.plot(price_area, scores)
+	path = folder+"/"+KPI+"_price_area.png"
+	plt.savefig(path)
+	print("    Saved to "+path)
+
+
+def mode_choice_wrt_price_area(samples, standards, KPI, name, folder):
+	return;
+
