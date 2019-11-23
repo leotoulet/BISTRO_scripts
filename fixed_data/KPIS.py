ec002a722b2e177b68a16ee02b1cbeaafb8d22bb
leotoulet <leo_toulet@berkeley.edu>
2019-11-22 19:55:55 -0800

    Added cost burden kpi

--- fixed_data/KPIS.py
+++ fixed_data/KPIS.py
@@ -53,8 +53,8 @@ Bus_crowding_KPI = {
 	"busCrowding":1
 }
 
-ALL_KPIS = [aggregate_KPI, aggregate_KPI_2, congestion_KPI, social_KPI, TollRevenue_KPI, Avg_vehicule_delay_KPI]
+ALL_KPIS = [aggregate_KPI, aggregate_KPI_2, cost_burden_KPI, congestion_KPI, social_KPI, TollRevenue_KPI, Avg_vehicule_delay_KPI]
 ALL_KPIS += [Avg_cost_burden_work_KPI, Avg_cost_burden_secondary_KPI, Bus_crowding_KPI]
 
-ALL_NAMES = ["Aggregate", "Aggregate_2", "Congestion", "social", "toll_revenue", "Avg_vehicule_delay"]
+ALL_NAMES = ["Aggregate", "Aggregate_2", "Cost burden" "Congestion", "social", "toll_revenue", "Avg_vehicule_delay"]
 ALL_NAMES += ["Avg_cost_burden_work", "Avg_cost_burden_secondary", "Bus_crowding"]
