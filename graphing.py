2846af52e7f5a844802a1bffad902adad3129bd7
leotoulet <leo_toulet@berkeley.edu>
2019-11-23 20:35:33 -0800

    Devel. link coloring

--- graphing.py
+++ graphing.py
@@ -82,7 +82,7 @@ def link_coloring():
 	saving_dir = CONFIG["OUTPUT_DIR"] + "/link_coloring"
 	os.makedirs(saving_dir, exist_ok = True)
 	for k,n in zip(KPIS, KPIS_names):
-		#best_scores_link_congestion(samples_copy, standards, k, n, saving_dir)
+		best_scores_link_congestion(samples_copy, standards, k, n, saving_dir)
 		best_scores_link_tolls(samples_copy, standards, k, n, saving_dir)
 
 def pareto():
