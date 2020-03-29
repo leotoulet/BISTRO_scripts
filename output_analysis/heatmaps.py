1f631439e1b25ea8bfcb950ce83aedafd83a96d0
leotoulet <leo.toulet@live.fr>
2020-03-29 16:21:18 -0700

    Adding SFlight to analysis

--- output_analysis/heatmaps.py
+++ output_analysis/heatmaps.py
@@ -13,11 +13,11 @@ sys.path.append(os.path.abspath("../utilities/"))
 from Sample import *
 from collect_inputs import *
 
-MIN_X = 676949
-MAX_X = 689624
+MIN_X = 540090
+MAX_X = 565829
 
-MIN_Y = 4818750
-MAX_Y = 4832294
+MIN_Y = 4163388
+MAX_Y = 4192081
 
 SLOPE = 1/100
 
