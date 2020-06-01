33b3145ac752cbdd052a36d51c60ac5cbf9045ec
leotoulet <leo.toulet@live.fr>
2020-06-01 14:12:12 -0700

    Adding laffer with all KPIS

--- output_analysis/laffer.py
+++ output_analysis/laffer.py
@@ -145,7 +145,7 @@ def plot_laffer(samples, standards, folder, KPIS, KPIS_names):
 		plt.legend(["Laffer points", "Best for Agg 0,1,2", "Best for Agg 3,4,5,7", "Best for Agg 6,8"])
 		plt.title(kn + " as a function road pricing")
 		plt.xlabel("road price x average tolled VMT per trip ($)")
-		plt.ylable(kn)
+		plt.ylabel(kn)
 
 		plt.savefig(folder+"/laffer_"+kn+".png")
 		print("    Saved " + kn + "road pricing curve plot to: "+folder+"/laffer_"+kn+".png")
