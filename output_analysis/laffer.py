fb59f55c7d3ddb824dfe8130ff41297cf7b9b511
leotoulet <leo.toulet@live.fr>
2020-06-01 13:34:27 -0700

    Adding laffer with all KPIS

--- output_analysis/laffer.py
+++ output_analysis/laffer.py
@@ -118,7 +118,10 @@ def plot_laffer(samples, standards, folder, KPIS, KPIS_names):
 		price = s.road_pricing["p"]
 		tr = s.KPIS["TollRevenue"][-1]
 		vmt = s.KPIS["VMT"][-1]
-		etr = vmt/(tr/price)
+
+		etr = 0.0
+		if tr > 0:
+			etr = price*vmt/tr
 		samples_etr[s] = etr
 
 	for k,kn in zip(KPIS, KPIS_names):
