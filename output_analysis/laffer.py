7e05bbf71b6c62f19b2cce81def95fa75031547a
leotoulet <leo.toulet@live.fr>
2020-06-05 09:59:55 -0700

    Adding unstd laffers

--- output_analysis/laffer.py
+++ output_analysis/laffer.py
@@ -19,6 +19,13 @@ df = pd.read_csv(file, index_col = "KPI")
 # graphing.py: added laffer, commented out other graphs
 # laffer.py: created file
 
+def colors_iter():
+    colors = ['o','m','g','r']
+    i = 0
+    while True:
+        yield colors[i%len(colors)]
+        i += 1
+
 def compute_laffer(samples, standards):
 	global sample_index
 	sample_index = 0
@@ -155,7 +162,7 @@ def plot_laffer_std(samples, standards, folder, KPIS, KPIS_names):
 		legend = []
 		for k in dic:
 			v = dic[k]
-			plt.plot(samples_etr[k], computeWeightedScores(k, standards, kpi)[-1], 'o')
+			plt.plot(samples_etr[k], computeWeightedScores(k, standards, kpi)[-1], 'o' + colors_iter())
 			l = "Best for agg "
 			for n in v:
 				l += str(n) + " "
@@ -206,7 +213,7 @@ def plot_laffer_unstd(samples, standards, folder, KPIS, KPIS_names):
 	legend = []
 	for k in dic:
 		v = dic[k]
-		plt.plot(samples_etr[k], k.KPIS["TollRevenue"][-1], 'o')
+		plt.plot(samples_etr[k], k.KPIS["TollRevenue"][-1], 'o' + colors_iter())
 		l = "Best for agg "
 		for n in v:
 			l += str(n) + " "
@@ -235,7 +242,7 @@ def plot_laffer_unstd(samples, standards, folder, KPIS, KPIS_names):
 	legend = []
 	for k in dic:
 		v = dic[k]
-		plt.plot(samples_etr[k], k.KPIS["VMT"][-1], 'o')
+		plt.plot(samples_etr[k], k.KPIS["VMT"][-1], 'o' + colors_iter())
 		l = "Best for agg "
 		for n in v:
 			l += str(n) + " "
@@ -266,7 +273,7 @@ def plot_laffer_unstd(samples, standards, folder, KPIS, KPIS_names):
 		legend = []
 		for k in dic:
 			v = dic[k]
-			plt.plot(samples_etr[k], k.raw_data[kpi], 'o')
+			plt.plot(samples_etr[k], k.raw_data[kpi], 'o'+ colors_iter())
 			l = "Best for agg "
 			for n in v:
 				l += str(n) + " "
